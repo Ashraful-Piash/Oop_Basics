a7ed4c4e206584b04ec42ac3bc95de6a5f9285ed
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-05-28 00:01:44 +0600

    edited

--- src/com/piash/helper_gfg_class_method.java
+++ src/com/piash/helper_gfg_class_method.java
@@ -2,10 +2,9 @@ package com.piash;
 public class helper_gfg_class_method {
     public static void main(String[]args){
         gfg_addition_method add = new gfg_addition_method();
-        int s = add.addTwoInt(5,6);
+        int s = add.addTwoInt(8,10);
         System.out.println("Sum of two integer values: "+s);
 
         }
-
     }
 
