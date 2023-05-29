88aa9fba115c5fdc7b9f597d7fdff1c5bdf85ad8
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-05-29 20:49:41 +0600

    initialized

---
+++ src/com/piash/Encap.java
@@ -0,0 +1,14 @@
+package com.piash;
+
+public class Encap {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+
+    }
+}
---
+++ src/com/piash/TestEncap.java
@@ -0,0 +1,11 @@
+package com.piash;
+
+public class TestEncap {
+    public static void main(String[]args){
+        Encap ob1 = new Encap();
+        ob1.setName("Ashraful Piash");
+        System.out.println(ob1.getName());
+
+    }
+    
+}
