de84572d11637addce52e79a8c8a6e400e42d4f5
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-05-31 13:06:04 +0600

    edited

--- src/com/piash/GuessGame.java
+++ src/com/piash/GuessGame.java
@@ -4,10 +4,12 @@ public class GuessGame {
     String p1;
     String p2;
     String p3;
+    String p4;
     public void startGame() {
         p1 = String.valueOf(new Player());
         p2 = String.valueOf(new Player());
         p3 = String.valueOf(new Player());
+        p4 = String.valueOf(new Player());
 
     }
 
