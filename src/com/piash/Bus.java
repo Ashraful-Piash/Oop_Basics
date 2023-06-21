41af713fe814aa4c8f7cb63213b0a9ba5348ad1c
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-06-21 20:06:41 +0600

    edited

--- src/com/piash/Bus.java
+++ src/com/piash/Bus.java
@@ -1,17 +1,17 @@
 package com.piash;
 
-public class Bus extends Vehicle{
+public class Bus extends Vehicle {
     int gear;
 
-    Bus(String name, String color, Double weight, int gear){
-            super(name,color,weight);
-            this.gear = gear;
+    Bus(String name, String color, Double weight, int gear) {
+        super(name, color, weight);
+        this.gear = gear;
     }
 
     @Override
-    void displayAttribute(){
+    void displayAttribute() {
         super.displayAttribute();
-        System.out.println("Gear: "+gear);
+        System.out.println("Gear: " + gear);
         System.out.println("ok");
         System.out.println("Need to be consistent");
         System.out.println("Allah , please forgive me and guide me to the right path");
