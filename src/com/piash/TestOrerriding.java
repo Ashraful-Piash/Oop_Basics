faa3ff3c50901121185678448cf1bd84ad999c7e
Ashraful-Piash <piashrafulhaque@gmail.com>
2023-07-11 12:57:17 +0600

    modified

--- src/com/piash/TestOrerriding.java
+++ src/com/piash/TestOrerriding.java
@@ -2,12 +2,15 @@ package com.piash;
 
 public class TestOrerriding {
     public static void main(String[] args) {
-        Shape s = new Shape();
-        Rectangle r  = new Rectangle(5,10);
-        Triang t = new Triang(10,5);
 
-        System.out.println(s.area());
-        System.out.println(r.area());
-        System.out.println(t.area());
+        Shape [] s = new Shape[3];
+        s[0]= new Shape();
+        s[1]= new Rectangle(10,5);
+        s[2]= new Triang(5,10);
+
+        for(int i = 0; i < 3; i++){
+            System.out.println(s[i].area());
+        }
+
     }
 }
